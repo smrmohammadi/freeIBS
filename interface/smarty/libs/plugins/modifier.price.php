81bc381637fd7399fd5d46056b8f295dc164c6be
farshad_kh <farshad_kh>
2005-01-06 09:57:42 +0000

    fix bug with some specially formatted numbers and floating point numbers

--- interface/smarty/libs/plugins/modifier.price.php
+++ interface/smarty/libs/plugins/modifier.price.php
@@ -7,15 +7,16 @@ function smarty_modifier_price($string)
 	$sign=$price<0?-1:1;
 	$price*=$sign;
 	$int_price=(int)$price;
+	$int_part="{$int_price}";
 	$float_part=round(($price-$int_price)*100);
 	$str="";
-	while($int_price>1000)
+	while(strlen($int_part)>3)
 	{
-	    $part=$int_price%1000;
-	    $int_price=(int)($int_price/1000);
+	    $part=substr($int_part,strlen($int_part)-4,3);
+	    $int_part=substr($int_part,0,strlen($int_part)-3);
 	    $str=",{$part}{$str}";
 	}
-	$str="{$int_price}{$str}";
+	$str="{$int_part}{$str}";
 	if($float_part>0)
 	    $str.=".{$float_part}";
 	if($sign==-1)
